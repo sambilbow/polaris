f9d33df218666f738ca63b36750e6084643fb6fa
Sam Bilbow <31018239+sambilbow@users.noreply.github.com>
2021-08-25 17:19:52 +0100

    remove hand rays and update experience_0

---
+++ Assets/Scripts/polaris/handRays.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class handRays : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PointerUtils.SetGazePointerBehavior(PointerBehavior.AlwaysOff);
+        PointerUtils.SetHandRayPointerBehavior(PointerBehavior.AlwaysOff);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
