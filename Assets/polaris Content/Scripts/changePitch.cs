b6ec4768be239014d087bf8635f49a60d8542a6c
Sam Bilbow <31018239+sambilbow@users.noreply.github.com>
2022-04-29 21:57:06 +0100

    Add emh_sussex experience

---
+++ Assets/polaris Content/Scripts/changePitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Freya;
+
+public class changePitch : MonoBehaviour
+{
+    analogPinValues analogPinValues;
+    float pitchValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        analogPinValues = GameObject.Find("Arduino Nano").GetComponent<analogPinValues>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pitchValue = Mathfs.RemapClamped(60f, 130f, 0.2f, 0.3f, analogPinValues.heartRate);
+        transform.GetComponent<AudioSource>().pitch = pitchValue;
+    }
+}
